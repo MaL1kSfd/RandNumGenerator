c51930600a3c5755b37d0c805abfd7a5c5f14614
MaL1kSfd <144499813+MaL1kSfd@users.noreply.github.com>
2026-03-17 23:38:50 +0200

    2 new functions added + some minor stuff
    SaveRNs func added - saves the random numbers from the list into a txt file
    ListToTable (Not done) - meant to take the values from the file and sort them into a table
    .... other stuff ....

--- CMPE485Py/test.py
+++ CMPE485Py/test.py
@@ -1,25 +1,48 @@
-seed = 27
-count = 10000
+from prettytable import PrettyTable
 
-randomNumBetweenZeroAndOne = []
+seed = 27
+count = 1000
+
+randomNum = []
+
+randoms = PrettyTable(["Bin No.", "Bin Lower Limit", "Bin Higher Limit", "Bin-Count"])
 
 def RandomGen(x, count):
     nextRand = 0
-    a = 101; c = 3; m = 10000 #make c and m coprime
+    a = 101; c = 3; m = 10000 
 
     for i in range(count): 
         #Xi+1
         nextRand = (a*x+c) % m
         x = nextRand
 
-        #randomNumBetweenZeroAndOne[i] = nextRand/m
-        randomNumBetweenZeroAndOne.append(nextRand/m)
+        randomNum.append(nextRand/m)
 
-        print(nextRand)
+        #print(nextRand)
+
+file_path = "RN-file.txt"
+
+def ListToTable():
+    for rn in randomNum:
+        match randomNum[rn]:
+            case 1:
+                print("Pip")
+
+    print("Stuff")
+
+def SaveRNs():
+    try:
+        with open(file_path, "w") as file:
+            for rn in randomNum:
+                file.write(str(rn) + " ")
+        print(f"txt file '{file_path}' was created")
+    except FileExistsError:
+        print("That file already exists!")
 
 
 RandomGen(seed, count)
-print(randomNumBetweenZeroAndOne)
+print(randomNum)
+SaveRNs()
 
 #do chi-square test
 
